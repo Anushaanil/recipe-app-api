2097afaaf2d8b8996ed57d705f85946a79cc8ec8
Anusha Shet <anushaanil44@gmail.com>
2022-09-21 13:00:37 +0530

    implementing TDD using calc methods

---
+++ app/app/tests.py
@@ -0,0 +1,20 @@
+""" 
+Sample Tests 
+
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTest(SimpleTestCase):
+    def test_add_numbers(self):
+        """ Test the add module """
+
+        res = calc.add(5,10)
+        self.assertEqual(res,15)
+    
+    def test_subtract_numbers(self):
+        """ Test the subtract module """
+
+        res = calc.subtract(5,10)
+        self.assertEqual(res,5)
